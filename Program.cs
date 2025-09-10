795b9caeace3d826c63e324fed7e10d45918dea2
Mohammed-cell-gif <moal08008@utb.vaxjo.se>
2025-09-10 12:40:55 +0200

    Programmeringuppgift1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+string str;
+Console.WriteLine("Skriv ett tall");
+str = Console.ReadLine();
+int tal = int.Parse(str);
+Console.WriteLine("Du skrev:" + tal);
+string str2;
+str2 = Console.ReadLine();
+int tal2 = int.Parse(str2);
+Console.WriteLine("Du skrev" + tal2);
+Console.WriteLine(tal + tal2);
+Console.WriteLine(tal - tal2);
+*/
+
+Console.WriteLine("Vad heter du?");
+string str;
+str = Console.ReadLine();
+Console.WriteLine(str);
